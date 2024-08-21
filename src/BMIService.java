f20c76750737701e09f73b9061415a188474e952
Natalia Korobchenko <tasha.tviko@gmail.com>
2024-08-21 17:26:16 +0300

    lines removed

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,10 +1,5 @@
 public class BMIService {
 
-    int weight;
-    double height;
-    int index;
-
-
     public int calculate(int weight, double height) {
         int index = (int) (weight / (height * height));
         return (int) index;
